d1453bb737d2ce1d68f91e15d1027ee92b3066b4
Catalin Niculescu <extern.constantin.niculescu@tttech-auto.com>
2021-05-03 23:12:12 +0300

    Make use of API from coinmarketcapapi to get Eth price

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,9 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 # from tracker import get_prices
-import cryptocompare
+# import cryptocompare
+import coinmarketcapapi
+from coinmarketcapapi import CoinMarketCapAPI, CoinMarketCapAPIError
 
 telegram_bot_token = "1736568748:AAFybS5euK76Ec2JNueYSN8jy8439MGhZDw"
 
@@ -22,27 +24,32 @@ dispatcher = updater.dispatcher
 #         message += f"Coin: {coin}\nPrice: ${price:,.2f}\nHour Change: {change_hour:.3f}%\nDay Change: {change_day:.3f}%\n\n"
 #         return message
 
-command_list = "/start\n/stop\n/eth" #TODO: make it tuple of callbaks and text
+command_list = "/start\n/stop-bot\n/eth-price\n" #TODO: make it tuple of callbaks and text
 
 def help_list(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=command_list)
 
-def start(update, context):
-    chat_id = update.effective_chat.id
-    message = "Hai sa traiesti! Aici botu' tau personal ;)"
-    # used the send_message method provided by the Telegram library to send messages to our users 
-    # when they expect a response. The method takes in a parameter called chat_id that is uniquely assigned to every Telegram user along with the message we want to send.
-    context.bot.send_message(chat_id=chat_id, text=message)
+# def start(update, context):
+#     chat_id = update.effective_chat.id
+#     message = "Hai sa traiesti! Aici botu' tau personal ;)"
+#     # used the send_message method provided by the Telegram library to send messages to our users 
+#     # when they expect a response. The method takes in a parameter called chat_id that is uniquely assigned to every Telegram user along with the message we want to send.
+#     context.bot.send_message(chat_id=chat_id, text=message)
 
-def stop(update, context):
+def stop_bot(update, context):
     updater.stop()
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Bot stopped")
+
+# def eth_price(update, context):
+#     # exchange = 'binancedex'
+#     # eth_usdt = cryptocompare.get_avg('ETH', currency='USD', exchange=exchange)
+#     eth_usd = cryptocompare.get_price('ETH', currency='USD', full=False)
+#     # eth_usd = "1 Eth = %d USD\nMarket: %d\n", eth_usd, "Binance"
+#     context.bot.send_message(chat_id=update.effective_chat.id, text=eth_usd)
 
 def eth_price(update, context):
-    # exchange = 'binancedex'
-    # eth_usdt = cryptocompare.get_avg('ETH', currency='USD', exchange=exchange)
-    eth_usd = cryptocompare.get_price('ETH', currency='USD', full=False)
-    # eth_usd = "1 Eth = %d USD\nMarket: %d\n", eth_usd, "Binance"
-    context.bot.send_message(chat_id=update.effective_chat.id, text=eth_usd)
+    r = CoinMarketCapAPI('{a3301629-efa1-417b-88e5-59bcef11d14b}').cryptocurrency_info(symbol='BTC')
+    print('Ethereum price: ', r.data)
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
@@ -53,8 +60,8 @@ def caps(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 # A command handler is a block of code called when a bot user triggers a certain command
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("stop", stop))
+# dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("stop_bot", stop_bot))
 dispatcher.add_handler(CommandHandler("help", help_list))
 dispatcher.add_handler(CommandHandler("eth", eth_price))
 
